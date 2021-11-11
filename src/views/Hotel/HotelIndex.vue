f66e3bbd7a226c2a31d4f56d1c2903dca8bd63c7
hsinhui <shinhuei1@gmail.com>
2021-11-11 21:55:38 +0800

    增加定位功能

--- src/views/Hotel/HotelIndex.vue
+++ src/views/Hotel/HotelIndex.vue
@@ -2,26 +2,27 @@
   <div class="accommodation">
     <div class="container-fluid header-bg position-relative py-3">
       <div class="accommodation-bg">
-        <div class="container w-50 text-white">
-          <h2 class="h1 font-weight-bold">
+        <div class="container w-50 text-white" :class="{ 'w-100': isMobile }">
+          <h2 class="h1 font-weight-bold" :class="{ 'text-center': isMobile }">
             Welcome to <span class="text-primary">Taiwan°</span>
           </h2>
           <p class="subtitle-1 text-left">
-            台北、台中、台南、屏東、宜蘭……遊遍台灣
+            臺北、臺中、臺南、屏東、宜蘭……遊遍臺灣
           </p>
           <div class="d-flex align-items-center">
             <input
               type="text"
               class="form-control"
-              placeholder="請輸入想去的縣市"
-              aria-label="請輸入想去的縣市"
-              v-model="searchInput"
+              placeholder="請輸入想去的縣市 例: 臺中市"
+              aria-label="請輸入想去的縣市 例: 臺中市"
+              v-model.trim="searchInput"
               @keyup.enter="searchData"
             />
             <button
               type="button"
               class="btn btn-primary ml-2"
               @click="searchData"
+              :disabled="!searchInput"
             >
               <span class="mdi mdi-24px mdi-search-web"></span>
             </button>
@@ -36,18 +37,18 @@
                 <option selected value="catering">美食</option>
                 <option value="hotel">住宿</option>
               </select>
-              <select
-                class="custom-select select-icon"
-                v-model="citySelect"
-                @change="toCityDetail"
-              >
+              <select class="custom-select select-icon" v-model="citySelect">
                 <option selected value="all">不分縣市</option>
                 <option v-for="city in citiesName" :key="city" :value="city.en">
                   {{ city.ch }}
                 </option>
               </select>
             </div>
-            <button type="button" class="btn btn-warning ml-2 text-white">
+            <button
+              type="button"
+              class="btn btn-warning ml-2 text-white"
+              @click="locate"
+            >
               <span class="mdi mdi-24px mdi-map-marker"></span>
             </button>
           </div>
@@ -60,10 +61,14 @@
 </template>
 <script>
 import cities from '@/assets/cities.json'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'Accomodation',
   computed: {
+    ...mapGetters({
+      isMobile: 'isMobile'
+    }),
     citiesName () {
       const citiesOptions = []
       cities.forEach(item => {
@@ -88,6 +93,7 @@ export default {
     }
   },
   methods: {
+    ...mapActions(['setIsLoading']),
     toCityDetail () {
       if (this.categorySelect === 'catering') {
         this.$router.push({
@@ -107,6 +113,41 @@ export default {
         params: { category: this.categorySelect },
         query: { input: this.searchInput }
       })
+    },
+    locate () {
+      this.setIsLoading(true)
+      return new Promise(resolve => {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          const positionObj = {
+            lat: position.coords.latitude,
+            long: position.coords.longitude
+          }
+          resolve(positionObj)
+        })
+      }).then(res => {
+        this.getLocationCity(res.lat, res.long)
+      })
+    },
+    getLocationCity (lat, long) {
+      this.axios
+        .get(
+          `https://api.nlsc.gov.tw/other/TownVillagePointQuery/${long}/${lat}`
+        )
+        .then(res => {
+          const city = res.data.ctyName
+          const enCity = this.citiesName.find(item => {
+            return item.ch === city
+          })
+          this.citySelect = enCity.en
+          this.setIsLoading(false)
+        })
+    }
+  },
+  watch: {
+    citySelect: {
+      handler () {
+        this.toCityDetail()
+      }
     }
   }
 }
